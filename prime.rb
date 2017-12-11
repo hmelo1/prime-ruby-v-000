3586f9b93a85c4e64f4eb9a4a73d9e4114727ed1
Eddie Melo <heriberto@melo.nyc>
2017-12-11 03:45:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number == 2
+    true
+  elsif number == 3
+    true
+  elsif number % 2 == 0
+    false
+  elsif number % 3 == 0
+    false
+  end
+
+  i = 5
+  w = 2
+
+  while i * i <= number
+    if number % i == 0
+      false
+    end
+    i += w
+    w = 6 - w
+  end
+  true
+end
